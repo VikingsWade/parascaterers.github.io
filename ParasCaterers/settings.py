836e9f09b15b890e29d8daaf35ccdb4ab273bf4d
VikingsWade <schandresh24@gmail.com>
2023-09-05 14:51:30 +0530

    make changes in Settings.oy for Allowed hosts for DNS

--- ParasCaterers/settings.py
+++ ParasCaterers/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dtl@b#6^f+-+izwd-#*my-hvqs+%xxt0i%ufcgys0ejivd$6aq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.parascaterers.com', 'parascaterers.com', '127.0.0.1', '18.232.70.90', '172.31.45.105']
 
 
 # Application definition
